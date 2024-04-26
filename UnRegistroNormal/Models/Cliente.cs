74aadecac064a2da800dbc0f56f0586dc53cffbe
BrynOvalles <brynovallesburgos@gmail.com>
2024-04-26 00:24:14 -0400

    Creando el  modelo

---
+++ UnRegistroNormal/Models/Cliente.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UnRegistroNormal.Models;
+
+public class Cliente
+{
+	[Key]
+	public int ClienteId { get; set; }
+	[Required(ErrorMessage = "El campo {0} es Requerido.")]
+
+	public string? Nombre { get; set; }
+	[Required(ErrorMessage = "El campo {0} es requerido.")]
+	[RegularExpression("^[0-9]+$", ErrorMessage = "El Teléfono solo puede contener dígitos.")]
+	[StringLength(maximumLength: 10, MinimumLength = 10, ErrorMessage = "La longitud debe ser de 10 dígitos")]
+	public string? Telefono { get; set; }
+	[Required(ErrorMessage = "El campo {0} es requerido.")]
+	[RegularExpression("^[0-9]+$", ErrorMessage = "El Celular solo puede contener dígitos.")]
+	[StringLength(maximumLength: 10, MinimumLength = 10, ErrorMessage = "La longitud debe ser de 10 dígitos")]
+	public string? Celular { get; set; }
+	[Required(ErrorMessage = "El campo {0} es Requerido.")]
+	[RegularExpression("^[0-9]+$", ErrorMessage = "El RNC solo puede contener dígitos.")]
+	[StringLength(maximumLength: 9, MinimumLength = 9, ErrorMessage = "La longitud debe ser de 9 dígitos")]
+	public string? RNC { get; set; }
+	[Required(ErrorMessage = "El campo {0} es Requerido.")]
+	public string? Email { get; set; }
+	[Required(ErrorMessage = "El campo {0} es Requerido.")]
+	public string? Dirección { get; set; }
+
+}
