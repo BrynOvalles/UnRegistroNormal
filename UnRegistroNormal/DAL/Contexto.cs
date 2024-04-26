b0f48dce5f28bcf0f08c91d73e07864e59ba36ec
BrynOvalles <brynovallesburgos@gmail.com>
2024-04-26 00:22:43 -0400

    Creando Contexto

---
+++ UnRegistroNormal/DAL/Contexto.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UnRegistroNormal.Models;
+
+namespace UnRegistroNormal.DAL;
+
+public class Contexto : DbContext
+{
+	public Contexto(DbContextOptions<Contexto> options) : base(options) {}
+	public DbSet<Cliente> clientes { get; set; }
+}
